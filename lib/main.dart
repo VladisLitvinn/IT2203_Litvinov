e51ef428eefcbeb8fb23a92d9ac2a9ab0be8cb79
Vladislav Litvinov <vladislav.litvinov.04@bk.ru>
2025-03-01 16:19:44 +0300

    Загрузка Flutter приложения

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Инкремент',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
+      ),
+      home: const MyHomePage(title: 'Инкремент :)'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Значение инкремента:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _decrementCounter,
+                  child: const Text('-'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  child: const Text('+'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20), // Отступ для кнопки сброса
+
+            // Прозрачная кнопка "Сбросить" с серым текстом
+            TextButton(
+              onPressed: _resetCounter,
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.transparent, // Прозрачный фон
+              ),
+              child: Text(
+                'Сбросить',
+                style: TextStyle(
+                  color: Colors.grey, // Серый текст
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
